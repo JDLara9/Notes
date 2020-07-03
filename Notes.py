248980fcb8771d295fc7650d872610d662893cd7
JDLara9 <36972778+JDLara9@users.noreply.github.com>
2020-07-03 13:45:56 -0500

    Added text box

--- Notes.py
+++ Notes.py
@@ -2,8 +2,8 @@ import tkinter as tk
 from tkinter import messagebox
 import sys
 
-# We can make a default font to be used here or be able to change it later
-# by going to settings in future sprint
+ # We can make a default font to be used here or be able to change it later
+ # by going to settings in future sprint
 DEFAULT_FONT = ('Verdana', 12)
 
 class App:
@@ -11,6 +11,7 @@ class App:
         self.root_window = tk.Tk(className = 'Notes')
         self.root_window.geometry("800x685")
         self.root_window['background']='#1e2024'
+
         self.show()
 
     def show(self):
@@ -18,11 +19,13 @@ class App:
         topFrame.pack()
         bottomFrame = tk.Frame(self.root_window)
         bottomFrame.pack(side=tk.BOTTOM)
+        text = tk.Text(self.root_window, fg = "gray", bg = '#1e2024')
+        text.pack()
 
 
-        #To create buttons
-        button1 = tk.Button(topFrame, text="Save", fg = "red")
-        button2 = tk.Button(topFrame, text="Open", fg = "blue")
+         #To create buttons
+        button1 = tk.Button(topFrame, text="Save", fg = "red", bg = '#1e2024')
+        button2 = tk.Button(topFrame, text="Open", fg = "blue", bg = '#1e2024')
         button3 = tk.Button(topFrame, text="Button 3", fg = "green")
         button4 = tk.Button(topFrame, text="Button 4", fg = "yellow")
 
