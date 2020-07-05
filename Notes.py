a9fa3a973f79781764f59240222e30d9d0b29f8f
Mario Rodriguez <marior6@uci.edu>
2020-07-05 18:17:08 -0500

    Added the top banner with options for File, Edit and Contact. Some options we still need to work on of course but just a way to do it. Deleted the buttons and changed color of text to white so its more visible..

--- Notes.py
+++ Notes.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import sys
+import webbrowser
 
  # We can make a default font to be used here or be able to change it later
  # by going to settings in future sprint
@@ -8,11 +9,36 @@ DEFAULT_FONT = ('Verdana', 12)
 
 class App:
     def __init__(self):
-        self.root_window = tk.Tk(className = 'Notes')
+        super().__init__()
+        self.root_window = tk.Tk()
+        self.root_window.title('NotePad')
         self.root_window.geometry("800x685")
-        self.root_window['background']='#1e2024'
+        self.root_window['background'] = '#1e2024'
 
         self.show()
+        self.setupUI()
+
+    def setupUI(self):
+        menuBar = tk.Menu(self.root_window)
+        self.root_window.config(menu=menuBar)
+
+        # Menu for File
+        fileMenu = tk.Menu(menuBar)
+        fileMenu.add_command(label="New", command=self.newNote)
+        fileMenu.add_command(label="Open", command=self.openNote)
+        fileMenu.add_command(label="Save", command=self.saveNote)
+        fileMenu.add_command(label="Exit", command=self.exitNote)
+        menuBar.add_cascade(label="File", menu=fileMenu)
+
+        # Menu for Edit
+        editMenu = tk.Menu(menuBar)
+        editMenu.add_command(label="Copy", command=self.onCopy)
+        menuBar.add_cascade(label="Edit", menu=editMenu)
+
+        # Menu for Contact
+        contactMenu = tk.Menu(menuBar)
+        contactMenu.add_command(label="Github", command=self.onGithub)
+        menuBar.add_cascade(label="Contact", menu=contactMenu)
 
     def show(self):
         topFrame = tk.Frame(self.root_window)
@@ -20,21 +46,30 @@ class App:
         bottomFrame = tk.Frame(self.root_window)
         bottomFrame.pack(side=tk.BOTTOM)
         text = tk.Text(self.root_window, fg = "gray", bg = '#1e2024')
+        text.config(font=DEFAULT_FONT, fg='white')
         text.pack()
 
+    def newNote(self):
+        # Thinking this opens up another NotePad window
+        messagebox.showwarning('Warning', 'Work in progress...')
 
-         #To create buttons
-        button1 = tk.Button(topFrame, text="Save", fg = "red", bg = '#1e2024')
-        button2 = tk.Button(topFrame, text="Open", fg = "blue", bg = '#1e2024')
-        button3 = tk.Button(topFrame, text="Button 3", fg = "green")
-        button4 = tk.Button(topFrame, text="Button 4", fg = "yellow")
+    def openNote(self):
+        # This will open an existing note, once we have a DB tho
+        messagebox.showwarning('Warning', 'Work in progress...')
 
-        button1.pack(side=tk.LEFT)
-        button2.pack(side=tk.LEFT)
-        button3.pack(side=tk.LEFT)
-        button4.pack(side=tk.LEFT)
+    def saveNote(self):
+        # Again once we have DB we can save work
+        messagebox.showwarning('Warning', 'Work in progress...')
 
-        messagebox.showinfo("Welcome to Notes", "Please feel free to type in your credit card information!")
+    def exitNote(self):
+        self.root_window.quit()
+
+    def onCopy(self):
+        # This will do a copy of highlighted area
+        messagebox.showwarning('Warning', 'Work in progress...')
+
+    def onGithub(self):
+        webbrowser.open('https://github.com/JDLara9/Notes')
 
     def start(self):
         self.root_window.mainloop()
